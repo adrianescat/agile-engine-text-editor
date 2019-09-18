55d5328dc4d1fcbf8a8ba760cea39a706c2c0288
adrianescat <adrian.escat@mercadolibre.com>
2019-09-18 19:18:45 -0300

    base functionality

--- src/components/control-panel/ControlPanel.js
+++ src/components/control-panel/ControlPanel.js
@@ -2,14 +2,63 @@ import React, { Component } from 'react';
 import '../../styles/ControlPanel.css';
 
 class ControlPanel extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      replacement: [],
+      word: '',
+      synonyms: [],
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.activeWordText !== this.props.activeWordText) {
+      this.findSynonyms();
+    }
+  }
+
+  findSynonyms = () => {
+    fetch('https://api.datamuse.com/words?rel_syn=' + this.props.activeWordText + '')
+      .then(res => res.clone().json())
+      .then((res) => {
+        const listOfSynonyms = [];
+        res.map((value) => {
+          listOfSynonyms.push(value.word);
+        });
+
+        this.setState({ synonyms: listOfSynonyms });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  handleSelect = e => {
+    this.setState({ select: e.target.value })
+  }
+
   render() {
+    const activeWordInfo = this.props.word;
+    const synonyms = this.state.synonyms.map((syn, index) => (<div key={index} className="syn" onClick={this.props.replace.bind(this,syn, this.props.activeWord)}>{syn}</div>));
     return (
-      <div id="control-panel">
+      <div id="control-panel" className="control-panel">
         <div id="format-actions">
-          <button className="format-action" type="button"><b>B</b></button>
-          <button className="format-action" type="button"><i>I</i></button>
-          <button className="format-action" type="button"><u>U</u></button>
+          <button className={`format-action ${activeWordInfo && activeWordInfo.bold ? 'active' : ''}`} type="button" onClick={e => this.props.makeBold(this.props.activeWord)}>
+            <b>B</b>
+          </button>
+          <button className={`format-action ${activeWordInfo && activeWordInfo.itallic ? 'active' : ''}`} type="button" onClick={e => this.props.makeItalic(this.props.activeWord)}>
+            <i>I</i>
+          </button>
+          <button className={`format-action ${activeWordInfo && activeWordInfo.underlined ? 'active' : ''}`} type="button" onClick={e => this.props.makeUnderlined(this.props.activeWord)}>
+            <u>U</u>
+          </button>
         </div>
+        {this.state.synonyms.length > 0 && <div className="synonyms">
+          <div className="label">Synonyms. Select to replace:</div>
+          <div className="box">
+            {synonyms}
+          </div>
+        </div>}
       </div>
     );
   }
--- src/components/file-zone/FileZone.js
+++ src/components/file-zone/FileZone.js
@@ -3,9 +3,13 @@ import '../../styles/FileZone.css';
 
 class FileZone extends Component {
   render() {
+    const { text } = this.props;
     return (
-      <div id="file-zone">
-        <div id="file">
+      <div id="file-zone" className="file-zone">
+        <div id="file" className="file">
+          <p className="predefined-text">
+            { text }
+          </p>
         </div>
       </div>
     );
