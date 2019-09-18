38d0487e7e89ebe927da3fd72e3ab65e9d419ddc
adrianescat <adrian.escat@mercadolibre.com>
2019-09-18 17:03:17 -0300

    new structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, {Component} from 'react';
-import './App.css';
-import ControlPanel from "./control-panel/ControlPanel";
-import FileZone from "./file-zone/FileZone";
-import getMockText from './text.service';
+import './styles/App.css';
+import ControlPanel from "./components/control-panel/ControlPanel";
+import FileZone from "./components/file-zone/FileZone";
+import getMockText from './tests/text.service';
 
 class App extends Component {
-    getText() {
-        getMockText().then(function (result) {
-            console.log(result);
-        });
-    }
-    render() {
-        return (
-            <div className="App">
-                <header>
-                    <span>Simple Text Editor</span>
-                </header>
-                <main>
-                    <ControlPanel/>
-                    <FileZone/>
-                </main>
-            </div>
-        );
-    }
+  getText() {
+    getMockText().then(function (result) {
+      console.log(result);
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <span>Best Text Editor</span>
+        </header>
+        <main>
+          <ControlPanel/>
+          <FileZone/>
+        </main>
+      </div>
+    );
+  }
 }
-
+  
 export default App;
+  
